f936d35b44a6a12465ac9b3b9b54f05a96a85a52
varshith-goud <varshithfit@gmail.com>
2025-07-12 22:03:11 +0530

    paths

--- src/mazeproblems/_2dpaths.java
+++ src/mazeproblems/_2dpaths.java
@@ -3,6 +3,7 @@ package mazeproblems;
 public class _2dpaths {
     public static void main(String[] args) {
         System.out.println(paths(3,3));
+        pathsprint("",3,3);
     }
     static int paths(int r,int c){
         if(r==1 || c==1 ){
@@ -12,5 +13,19 @@ public class _2dpaths {
         int right=paths(r,c-1);
 
         return left+right;
+    }
+
+    static void pathsprint(String p ,int r,int c){
+        if(r==1 && c==1 ){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            pathsprint(p+'d',r-1,c);
+        }
+        if(c>1){
+            pathsprint(p+'r',r,c-1);
+        }
+
     }
 }
