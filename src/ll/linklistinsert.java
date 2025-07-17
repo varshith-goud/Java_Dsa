93676abfb24a96b3277261d1803e2afe58934f48
varshith-goud <varshithfit@gmail.com>
2025-07-17 21:28:51 +0530

    new change

--- src/ll/linklistinsert.java
+++ src/ll/linklistinsert.java
@@ -5,12 +5,28 @@ public class linklistinsert {
         int arr[]={3,4,5,6,7};
         linklist head=convert(arr);
         linklist var=head;
+        int count=0;
         while (var!=null){
-            System.out.println(var.data+" ");
+            System.out.print(var.data+" ");
+            count++;
             var=var.next;
         }
+        System.out.println();
+        System.out.println(count);
+        System.out.println(present(head,5));
 
-        System.out.println(head.data);
+
+    }
+
+    static boolean present(linklist head,int val){
+        linklist temp=head;
+        while (temp!=null){
+            if(temp.data == val){
+                return true;
+            }
+            temp=temp.next;
+        }
+        return false;
     }
 
     static linklist convert(int []arr){
